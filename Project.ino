8780433c74bb9d9b15a8b1cb11bb0399c2247bf7
Phei-San <93628274+Phei-San@users.noreply.github.com>
2024-01-26 23:14:37 +0800

    Latest version

--- Project.ino
+++ Project.ino
@@ -6,23 +6,11 @@
 #include "DHT.h"
 #include <ESP32Servo.h> 
 
-int MinMoistureValue = 4095;
-int MaxMoistureValue = 1800;
-int MinMoisture = 0;
-int MaxMoisture = 100;
-int Moisture = 0;
-float min_soilMoisture = (100 - 3276.0/4095*100);  //min mositure value is 20%
-float max_soilMoisture = (100 - 2867.0/4095*100);  //max moisture value is 30%
-
 //define device id
-// const char* DHT11Sensor = "77ff2a45-62d1-4ac4-ac75-99f0f9dcb61e";     //Replace this with YOUR deviceID for the DHT11 sensor
-// const char* RainSensor = "4ddd7511-cdfb-40a3-b158-3117ad9d94c4";      //Replace this with YOUR deviceID for the rain sensor
-// const char* MoistureSensor = "4fd62586-8498-40c3-9ee9-3fd58425a5e1";  //Replace this with YOUR deviceID for the moisture sensor
-
-const char* DHT11Sensor = "cfb60689-2101-4854-9b44-1564578bf8d6";     //Replace this with YOUR deviceID for the DHT11 sensor
-const char* RainSensor = "2c42aab1-2ef5-49c6-9954-35ff26bb8bae";      //Replace this with YOUR deviceID for the rain sensor
-const char* MoistureSensor = "723c68c8-dbf9-4934-9dd1-30697d64f20c";  //Replace this with YOUR deviceID for the moisture sensor
-const char* AllSensor = "ffce5ae2-e0cf-44c6-8091-28d29c8aac7d";
+const char* DHT11Sensor = "77ff2a45-62d1-4ac4-ac75-99f0f9dcb61e";     //Replace this with YOUR deviceID for the DHT11 sensor
+const char* RainSensor = "4ddd7511-cdfb-40a3-b158-3117ad9d94c4";      //Replace this with YOUR deviceID for the rain sensor
+const char* MoistureSensor = "4fd62586-8498-40c3-9ee9-3fd58425a5e1";  //Replace this with YOUR deviceID for the moisture sensor
+const char* AllSensor = "fe5ba358-13e7-45b0-9720-7974d63346fb";
 
 //Used Pins
 const int DHT_PIN = A4;
@@ -30,6 +18,11 @@ const int DHT_TYPE = DHT11;
 const int rainPin = A5;
 const int soilPin = A2;
 int servoPin = 38;
+
+float min_soilMoisture = (100 - 3276.0/4095*100);  //min mositure value is 20%
+float max_soilMoisture = (100 - 2867.0/4095*100);  //max moisture value is 30%
+int cover = 0;
+int uncover = 120;
 
 //input sensor
 #define DHTTYPE DHT11
@@ -64,9 +57,6 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-
-
-
 void setup() {
 
   setup_wifi();
@@ -89,24 +79,24 @@ void loop() {
     voneClient.publishDeviceStatusEvent(MoistureSensor, true);
     voneClient.publishDeviceStatusEvent(AllSensor, true);
   }
-  
   voneClient.loop();
-
-  int cover = 120;
-  int uncover = 0;
 
   unsigned long cur = millis();
   if (cur - lastMsgTime > INTERVAL) {
     lastMsgTime = cur;
 
-    //Publish telemetry data 1
+    //Read sensor data
     float h = dht.readHumidity();
     int t = dht.readTemperature();
-    int raining = rainSensor();
+    int raining = !digitalRead(rainPin);
     int moistValue = soilSensor();
 
+    // if raining
+    // cover the plant is the soil is too moisture 
+    // uncover the plant if the soil is too dry
+    // else do nothing
     if(raining == HIGH) {
-      Serial.println("Raining");
+      Serial.println("Raining: 1");
       //if the soil is too dry
       if(moistValue < min_soilMoisture) {
         servo.write(uncover); //open
@@ -118,10 +108,10 @@ void loop() {
     }
     // no raining
     // cover the plant is the soil is too dry 
-    // uncover the plant if the soil is too wet
+    // uncover the plant if the soil is too moisture
     // else do nothing
     else {
-      Serial.println("Sunny");
+      Serial.println("Raining: 0");
       if (moistValue > max_soilMoisture) {
         servo.write(uncover); //open
       }
@@ -130,6 +120,7 @@ void loop() {
       }
     }
 
+    //Publish telemetry data 1
     JSONVar payloadObject, payloadObject2;
     payloadObject["Humidity"] = h;
     payloadObject["Temperature"] = t;
@@ -141,23 +132,22 @@ void loop() {
 
     //Publish telemetry data 2
     payloadObject["Raining"] = raining;
-    //voneClient.publishTelemetryData(RainSensor, payloadObject2);
     voneClient.publishTelemetryData(RainSensor, "Raining", raining);
 
     //Publish telemetry data 3
-    Moisture = map(moistValue, MinMoistureValue, MaxMoistureValue, MinMoisture, MaxMoisture);
+    //Moisture = map(moistValue, MinMoistureValue, MaxMoistureValue, MinMoisture, MaxMoisture);
     payloadObject["Soil moisture"] = moistValue;
-    //voneClient.publishTelemetryData(MoistureSensor, payloadObject3);
     voneClient.publishTelemetryData(MoistureSensor, "Soil moisture", moistValue);
 
     payloadObject2["Humidity"] = h;
     payloadObject2["Temperature"] = t;
     payloadObject2["Raining"] = raining;
-    payloadObject2["Soil moisture"] = moistValue;
-    voneClient.publishTelemetryData(AllSensor, payloadObject);
-  }
+    payloadObject2["Moisture"] = moistValue;
+    voneClient.publishTelemetryData(AllSensor, payloadObject2);
+    }
 }
 
+// highest value is 4095 - no rain
 int rainSensor() {
   float sensorValue = analogRead(rainPin);
   int raining = 1;
@@ -171,9 +161,10 @@ int rainSensor() {
   }
 }
 
+// highest value is 4095 - dry
 float soilSensor() {
   float sensorValue = analogRead(soilPin);  // Read the analog value from sensor
-  float soilMoisture = 100.0 - sensorValue/4095*100;
+  float soilMoisture = 100 - sensorValue/4095*100;
   Serial.print("Current Soil Moisture: ");
   Serial.println(soilMoisture);
   return soilMoisture;
